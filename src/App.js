f61585fa8b67af17679676fa1d38898c7a3fb155
Front <tanvanat112@gmail.com>
2024-08-18 09:47:18 +0700

    Updated src/App.js with [describe changes]

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 //jsx language
 function App() {
   const title = 'Welcome to Website Security Grading System';
-  const likes = 50;
+  const link = "";
 
   return (
-    <div className="App">
-      <div className='content'></div>
-      <h1>{title}</h1>
-      <p>Liked {likes} times</p>
-
+    <div className="flex justify-center items-center mt-10 mb-5">
+      <div className='content'>
+        <h1>{title}</h1>
+        <a href="{link}">
+          <img src="/logo192.png" alt="Logo" className="w-30 h-30 mt-20"/>
+        </a>
+      </div>
     </div>
   );
 }
