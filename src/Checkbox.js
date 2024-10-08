40d9a95b363ce90fff8b0e651a6138271fb5e7a1
tanvanat <106066148+tanvanat@users.noreply.github.com>
2024-10-08 15:39:42 +0700

    Delete src directory

--- src/Checkbox.js
+++
@@ -1,9 +0,0 @@
-const Checkbox = ({ name, isCheck }) => {
-    return (
-        <li className="item">
-            {isCheck ? name + ' ✅' : name}
-        </li>
-    );
-}
-
-export default Checkbox;
