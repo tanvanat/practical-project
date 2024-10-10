fec62a657fe9c6d1ecf6e1dd7554a248ebe2b57c
Front <tanvanat112@gmail.com>
2024-10-10 10:43:13 +0700

    learning express

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,9 +1,14 @@
-const express = require("express")
-
+const express = require("express");
 const app = express();
+
+app.set("view engine", "ejs")
+
 app.get('/', (req, res) => {
-    res.send("fdsa");
-})
+    res.render("index", {text: 'World'})
+});
+
 app.listen(8080, () => {
     console.log('server is listening on port 8080');
 });
+
+// เก็บ username,pwd ./front-end/src/Home.js
