d3a4dbfb0ad918b9183e7b13c3b932262caa7e36
Front <tanvanat112@gmail.com>
2024-10-12 20:18:08 +0700

    add

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -1,23 +1,32 @@
 import { Link } from 'react-router-dom';
 
 const Home = () => {
-    const title = 'Welcome to Website Security Grading System';
+    const title = 'Chat With Doctor';
     const link = "/Firstpage";
 
     return (
         <div className="home">
-            <div className="flex flex-col items-center min-h-screen text-center bg-black">
-                <div className='flex flex-col items-center pt-10 pb-40'>
-                    <h1 className="text-2xl font-bold text-blue-500 mb-10 hover:text-blue-800 transition-colors duration-300">
-                        {title}
-                    </h1>
-                    <Link to={link}>
-                        <img src="/dragon.png" alt="Logo" className="w-[350px] h-[350px] mt-10" />
-                    </Link>
-                </div>
-            </div>
+            <header class="bg-slate-200">
+                <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+                    <div class="flex lg:flex-1">
+                        <a href="#" class="-m-1.5 p-1.5">
+                            <span class="sr-only">Your Company</span>
+                            <img class="h-8 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600" alt=""></img>
+                        </a>
+                    </div>
+                    <div class="flex lg:hidden">
+                        <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+                            <span class="sr-only">Open main menu</span>
+                            <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                            </svg>
+                        </button>
+                    </div>
+                </nav>
+            </header>
+
         </div>
     );
-}
+};
 
 export default Home;
