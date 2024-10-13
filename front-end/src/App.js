6a78ab9d7ac0cf82443cb3dd4574892b37c5e2a3
Front <tanvanat112@gmail.com>
2024-10-13 16:09:01 +0700

    example.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,15 +4,16 @@ import Home from './Home';
 import FirstPage from './Firstpage'; 
 import Loading from './Loading';
 import Output from './Output';
+import Example from './Example';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Firstpage" element={<FirstPage />} />
+        <Route path="/" element={<Example />} />
+        {/* <Route path="/Firstpage" element={<FirstPage />} />
         <Route path="/Loading" element={<Loading />}/>
-        <Route path="/Output" element={<Output/>}/>
+        <Route path="/Output" element={<Output/>}/> */}
       </Routes>
     </Router>
   );
--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -1,9 +1,9 @@
 import { Link } from 'react-router-dom';
 
+
 const Home = () => {
     const title = 'Chat With Doctor';
     const link = "/Firstpage";
-
     return (
         <div className="home">
             <header class="bg-slate-200">
@@ -14,17 +14,41 @@ const Home = () => {
                             <img class="h-8 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600" alt=""></img>
                         </a>
                     </div>
-                    <div class="flex lg:hidden">
-                        <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-                            <span class="sr-only">Open main menu</span>
-                            <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                            </svg>
-                        </button>
+                    <div class="hidden lg:flex lg:gap-x-12">
+                        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Home</a>
+                        <div class="relative">
+                            <button
+                                type="button"
+                                class="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900"
+                            >
+                                Features
+                                <svg class="h-5 w-5 flex-none text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+                                    <path fill-rule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+                                </svg>
+                            </button>
+                            
+                            <div class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+                                <div class="py-1" role="none">
+                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-0">Account settings</a>
+                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-1">Support</a>
+                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-2">License</a>
+                                    <form method="POST" action="#" role="none">
+                                        <button type="submit" class="block w-full px-4 py-2 text-left text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-3">Sign out</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+
+                        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Our Doctors</a>
+                        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">FAQs</a>
+                        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Our Company</a>
+                        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Resources</a>
+                    </div>
+                    <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+                        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Talk to a doctor <span aria-hidden="true">&rarr;</span></a>
                     </div>
                 </nav>
             </header>
-
         </div>
     );
 };
